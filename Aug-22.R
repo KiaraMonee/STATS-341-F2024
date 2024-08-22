2f72973a6ed09ddf9bed5e0e872d589dc4f735f0
KiaraMonee <112020040+KiaraMonee@users.noreply.github.com>
2024-08-22 16:25:39 -0400

    Add files via upload

---
+++ Aug-22.R
@@ -0,0 +1,12 @@
+#Author: Kiara, Date: 08/22/24, Purpose: Test the measures of descriptive stats
+
+# How to caluclate standard deviation in R
+X <- c(1,2,3,4,5)
+sd(X)
+
+#How to calculate mean in R
+mean(X)
+
+#How to caluclate median in R
+median(X)
+
