c443aae37069cace06f85612cadc5be9915b10d3
KiaraMonee <112020040+KiaraMonee@users.noreply.github.com>
2024-09-17 19:18:33 -0400

    Add files via upload

---
+++ Aug-29.R
@@ -0,0 +1,12 @@
+Kiara Mikel ; August 29 2024 ; Create 1000 random numbers and store them in a variable called variable1.
+
+#Getting random numbers and storing them.
+
+variable1 <- runif(1000)
+
+sd_variable1 <- sd(variable1)
+
+mean_variable1 <- mean(variable1)
+
+median_variable1 <- median(variable1)
+
